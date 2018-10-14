1cf8b62b07014c1759616a3bfc1f18bb319018a0
unknown <rom.ben.ten@gmail.com>
2018-10-14 16:29:24 +0300

    first step

---
+++ PuzzleDevCom/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace PuzzleDevCom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LinkBox = new System.Windows.Forms.TextBox();
+            this.OK = new System.Windows.Forms.Button();
+            this.LinkPictureBox = new System.Windows.Forms.PictureBox();
+            this.unsort = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.LinkPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LinkBox
+            // 
+            this.LinkBox.Location = new System.Drawing.Point(69, 60);
+            this.LinkBox.Name = "LinkBox";
+            this.LinkBox.Size = new System.Drawing.Size(377, 20);
+            this.LinkBox.TabIndex = 0;
+            // 
+            // OK
+            // 
+            this.OK.Location = new System.Drawing.Point(491, 32);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(63, 48);
+            this.OK.TabIndex = 1;
+            this.OK.Text = "next ";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // LinkPictureBox
+            // 
+            this.LinkPictureBox.Location = new System.Drawing.Point(1, 0);
+            this.LinkPictureBox.Name = "LinkPictureBox";
+            this.LinkPictureBox.Size = new System.Drawing.Size(331, 196);
+            this.LinkPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LinkPictureBox.TabIndex = 2;
+            this.LinkPictureBox.TabStop = false;
+            this.LinkPictureBox.Visible = false;
+            // 
+            // unsort
+            // 
+            this.unsort.Location = new System.Drawing.Point(396, 329);
+            this.unsort.Name = "unsort";
+            this.unsort.Size = new System.Drawing.Size(75, 23);
+            this.unsort.TabIndex = 8;
+            this.unsort.Text = "unsort";
+            this.unsort.UseVisualStyleBackColor = true;
+            this.unsort.Click += new System.EventHandler(this.unsort_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(198, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Give me image URL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(765, 304);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.unsort);
+            this.Controls.Add(this.LinkPictureBox);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.LinkBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.LinkPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LinkBox;
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.PictureBox LinkPictureBox;
+        private System.Windows.Forms.Button unsort;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PuzzleDevCom/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuzzleDevCom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static Image IMG_FROM_LINK;
+        public static List<int> randomList = new List<int>();
+        public static Random RANDOM = new Random();
+        public static int VerticalAmount = 2;
+        public static int GorizontalAmount = 2;
+        public static List<PictureBox> Puzzles = new List<PictureBox>();
+        public static List<PictureBox> NewBoxList = new List<PictureBox>();
+        public static Point MouseUpLocation;
+
+        public static int generator()
+        {
+            int aa = RANDOM.Next(0, VerticalAmount*GorizontalAmount);
+            while (randomList.Contains(aa) && randomList.Count < VerticalAmount * GorizontalAmount)
+            {
+                aa = RANDOM.Next(0, VerticalAmount*GorizontalAmount);
+            }
+            randomList.Add(aa);
+            return aa;
+        }
+        private void Split()
+        {
+            BoxBuilderAndRefact();
+            Bitmap originalImage = new Bitmap(IMG_FROM_LINK);
+            Rectangle rect;
+            Bitmap PartOfOriginal = originalImage;
+            int counter = 0;
+            for (int i = 0; i < VerticalAmount; ++i)
+                for (int j = 0; j < GorizontalAmount; ++j)
+                {
+                    rect = new Rectangle(j * originalImage.Width / GorizontalAmount, i * originalImage.Height / VerticalAmount, originalImage.Width / GorizontalAmount, originalImage.Height / VerticalAmount);
+                    PartOfOriginal = originalImage.Clone(rect, originalImage.PixelFormat);
+                    Puzzles[counter++].Image = PartOfOriginal;
+                }
+        }
+        private void BoxBuilderAndRefact()
+        {
+            PictureBox tmp;
+            for (int i = 0; i < VerticalAmount; ++i)
+            {
+                for (int j = 0; j < GorizontalAmount; ++j)
+                {
+                    {
+                        tmp = new PictureBox();
+                        tmp.Name = $"picturebox{i}";
+                        tmp.SizeMode = PictureBoxSizeMode.StretchImage;
+                        tmp.Location = new Point(500 + j * (LinkPictureBox.Width / GorizontalAmount + 3), i * (LinkPictureBox.Height / VerticalAmount + 3));
+                        tmp.Parent = this;
+                        //tmp.MouseDown += new MouseEventHandler(this.Mouse_Down);
+                        tmp.MouseMove += new MouseEventHandler(this.Mouse_Move);
+                        tmp.MouseUp += new MouseEventHandler(this.Mouse_Up);
+                        Puzzles.Add(tmp);
+                    }
+                }
+
+                foreach (var item in Puzzles)
+                {
+                    item.Width = LinkPictureBox.Width / GorizontalAmount;
+                    item.Height = LinkPictureBox.Height / VerticalAmount;
+                }
+            }
+        }
+        private void NewPictureBoxGeneration()
+        {
+            PictureBox newone;
+            for(int i = 0; i < GorizontalAmount; ++i)
+            {
+                for(int j = 0; j < VerticalAmount; ++j)
+                {
+                    newone = new PictureBox();
+                    newone.Size = Puzzles[0].Size;
+                    newone.SizeMode = PictureBoxSizeMode.StretchImage;
+                    newone.Location = new Point(500 + j * (Puzzles[0].Width + 3 ), i * (Puzzles[0].Height + 3));
+                    newone.BackColor = Color.White;
+                    newone.Parent = this;
+                    NewBoxList.Add(newone);
+                }
+            }
+        }
+        private void NecessaryBoxFinder( PictureBox Traveller)
+        {
+            foreach ( var item in NewBoxList)
+            {
+                if ( (item.Location.X < MouseUpLocation.X && (item.Location.X + item.Width)>MouseUpLocation.X)   && (item.Location.Y < MouseUpLocation.Y && (item.Location.Y + item.Height) > MouseUpLocation.Y)) {
+                    item.Name = Traveller.Name;
+                    item.BackColor = Color.Red;
+                    Traveller.Parent = this;
+                    item.Image = Traveller.Image;
+                    MessageBox.Show(item.Name);
+                    Traveller.Image = null;
+                }
+            }
+        }
+
+
+        private void Mouse_Up(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                MouseUpLocation = new Point(e.X + (sender as PictureBox).Left, e.Y + (sender as PictureBox).Top);
+            NecessaryBoxFinder((sender as PictureBox));
+        }
+        private void Mouse_Move(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                (sender as PictureBox).Left = e.X + (sender as PictureBox).Left - 50;
+                (sender as PictureBox).Top = e.Y + (sender as PictureBox).Top - 50;
+            }
+        }
+        private void OK_Click(object sender, EventArgs e)
+        {
+            this.Size = new Size(1000, 500);
+            label1.Visible = false;
+            LinkBox.Visible = false;
+            OK.Visible = false;
+            LinkPictureBox.Visible = true;
+            using (WebClient webClient = new WebClient())
+            {
+                byte[] data = webClient.DownloadData("https://images.wallpaperscraft.ru/image/fon_yarkiy_svet_oskolki_85568_1280x720.jpg");
+
+                using (MemoryStream mem = new MemoryStream(data))
+                    IMG_FROM_LINK = Image.FromStream(mem);
+                LinkPictureBox.Image = IMG_FROM_LINK;
+            }
+           
+            //pictureBoxes.AddRange(new List<PictureBox> { pictureBox1, pictureBox2, pictureBox3, pictureBox4 });
+            Split();
+        }
+        private void unsort_Click(object sender, EventArgs e)
+        {
+            foreach (PictureBox item in Puzzles)
+                item.Location = new Point(40 + generator() * (Puzzles[0].Width + 10), 250);
+            unsort.Visible = false;
+            NewPictureBoxGeneration();
+        }
+    }
+}
